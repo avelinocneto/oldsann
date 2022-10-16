611bc394332c9cf65f34c66c134fa6841eb32651
Avelino Neto <avelinocarmanhanneto@gmail.com>
2022-10-16 16:07:04 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const request = require("request");
+const express = require('express')
+const app = express()
+const port = 80
+const mods = [];
+// Create a test GET request to understand what the JSON response from the tmi API looks like
+request('http://tmi.twitch.tv/group/user/oldsann/chatters', {
+    json: true
+}, (err, res, body) => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log(body.chatter_count);
+    for (i = 0; body.chatters.moderators[i] != undefined; i++) {
+        mods.push(body.chatters.moderators[i]);
+    }
+});
+
+app.get('/', (req, res) => {
+  res.send(mods);
+})
+
+app.listen(port);
