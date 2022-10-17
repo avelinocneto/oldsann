c13f415cda96b2929fbd3b1df44c203502450ff8
Avelino Neto <avelinocarmanhanneto@gmail.com>
2022-10-17 14:29:19 -0300

    ajuste

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const request = require("request");
 const express = require('express')
 const app = express()
 const mods = [];
-const port = 3000;
+const port = process.env.PORT || 3001;
 // Create a test GET request to understand what the JSON response from the tmi API looks like
 request('http://tmi.twitch.tv/group/user/oldsann/chatters', {
     json: true
@@ -21,6 +21,4 @@ app.get('/mods', (req, res) => {
   res.send(mods);
 })
 
-app.listen(port, function (){
-    console.log('app running on port 3000');
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
