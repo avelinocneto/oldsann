d7625ef9204c3a5eedc8a5e05a1dfc4085d0e9d3
Avelino Neto <avelinocarmanhanneto@gmail.com>
2022-10-18 11:14:23 -0300

    ajustes

---
+++ app.js
@@ -0,0 +1,26 @@
+const request = require("request");
+const express = require('express')
+const app = express()
+const mods = [];
+const port = process.env.PORT || 3001;
+
+// Create a test GET request to understand what the JSON response from the tmi API looks like
+request('http://tmi.twitch.tv/group/user/oldsann/chatters', {
+    json: true
+}, (err, res, body) => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log(body.chatter_count);
+    for (i = 0; body.chatters.moderators[i] != undefined; i++) {
+        mods.push(body.chatters.moderators[i]);
+    }
+    console.log('mods added');
+});
+
+app.get("/", (req, res) => res.type('html').send("Hello World!"));
+app.get('/mods', (req, res) => {
+  res.send(mods);
+})
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
